f6aedc051d239ba078e7ba3ec79cecd66a61a73f
Brian Campbell <brian.campbell003@gmail.com>
2023-08-14 15:59:29 -0500

    word-adder front end working

--- client/src/components/pages/WordAdder.js
+++ client/src/components/pages/WordAdder.js
@@ -3,67 +3,42 @@ import './WordAdder.css';
 
 function WordAdder() {
     const [word, setWord] = useState('');
-    const [wordData, setWordData] = useState([]);
 
     const fetchData = (word) => {
         fetch('https://www.dictionaryapi.com/api/v3/references/collegiate/json/' + word + '?key=4013f53d-9186-47c1-acb8-17287689092c')
                 .then((response) => response.json())
                 .then((data) => {
                     console.log(data);
-                    setWordData(data);
+                        let word = data[0].meta.id;
+                        let partOfSpeech = data[0].fl;
+                        let etymology = data[0].et[0][1];
+                        let definition1 = data[0].shortdef[0];
+                        let definition2 = data[0].shortdef[1];
+                        let definition3 = data[0].shortdef[2];
+                    console.log('YOUR WORD IS', word, "WHICH MEANS", definition1, 'OR', definition2, 'OR', definition3, ". IT IS A ", partOfSpeech, "WITH ORIGINS ", etymology);
                 })
                 .catch((err) => {
                     console.log(err.message);
                 });
     }
 
-    function handleSearch(word) {
-        
+    function handleSearch(e) {
+        e.preventDefault();
         console.log("handling a search here");
         fetchData(word);
     }
 
-    // let searchBtnHandler = (event) => {
-    //     console.log('button click');
-    //     event.preventDefault();
-    //     let wordInputEl = document.querySelector("#wordName").value;
-    //     console.log(wordInputEl);
-
-    //     let wordUrl = 'https://www.dictionaryapi.com/api/v3/references/collegiate/json/' + wordInputEl + '?key=4013f53d-9186-47c1-acb8-17287689092c';
-
-    //     fetch(wordUrl)
-    //         .then(function (response) {
-    //             if (response.ok) {
-    //                 response.json().then(function (data) {
-    //                     console.log(data);
-    //                     let word = data[0].meta.id;
-    //                     let partOfSpeech = data[0].fl;
-    //                     let etymology = data[0].et[0][1];
-    //                     let definition1 = data[0].shortdef[0];
-    //                     let definition2 = data[0].shortdef[1];
-    //                     let definition3 = data[0].shortdef[2];
-    //                     console.log('YOUR WORD IS', word, "WHICH MEANS", definition1, 'OR', definition2, 'OR', definition3, ". IT IS A ", partOfSpeech, "WITH ORIGINS ", etymology);
-    //                 });
-    //             } else {
-    //                 // ADD alert modal
-    //             }
-    //         })
-
-    // }
-
-    // searchBtn.addEventListener('click', searchBtnHandler);
-
     return (
         <div>
             <h1>Add a word to your pool</h1>
-            <form id="word-form">
+            <form className="word-form" onSubmit={handleSearch}>
                 <input className="wordName" placeholder="Lookup your word" value={word} onChange={e => setWord(e.target.value)} />
-                <button type="submit" className="searchBtn" onClick={handleSearch}>Search</button>
+                <button type="submit" className="searchBtn">Search</button>
             </form>
 
-            <div id="wordResult">
-                <p id="returnedWord"></p>
-                <p id="returnedMoreDate"></p>
+            <div className="wordResult">
+                <p className="returnedWord"></p>
+                <p className="returnedMoreDate"></p>
 
             </div>
         </div>
