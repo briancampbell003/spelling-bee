79d267ff1230f17228f6a4672c737b3b4da3d496
Brian Campbell <brian.campbell003@gmail.com>
2023-09-11 17:54:17 -0500

    save new words work started

---
+++ client/src/components/pages/WordAdder/NewWord.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import './WordAdder.css';
+
+function NewWord(props) {
+    // const [word, setWord] = useState('');
+
+    function handleChange(e) {
+        // setWord(e.target.value);
+    }
+
+    function handleSave(e) {
+        e.preventDefault();
+        console.log("saving a word here");
+            // if (newDream) {
+            //     const response = await fetch('/api/dreams', {
+            //       method: 'POST',
+            //       body: JSON.stringify(newDream),
+            //       headers: { 'Content-Type': 'application/json' },
+            //     });
+            
+            //     if (response.ok) {
+            //       document.location.replace('/mydreams');
+            //     } else {
+            //       alert('Failed to post your new dream.');
+            //     }
+            //   }
+    }
+
+    return (
+        <>
+            <form className="word-form" onSubmit={handleSave}>
+                <input className="word2save" placeholder={props[0].meta.id} />
+                <button type="submit" className="saveWordBtn">Search</button>
+            </form>
+        </>
+    )
+}
+
+export default NewWord;
--- client/src/components/pages/WordAdder/WordAdderForm.js
+++ client/src/components/pages/WordAdder/WordAdderForm.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import NewWord from './NewWord';
 import './WordAdder.css';
 
 function WordAdderForm(props) {
@@ -8,6 +9,7 @@ function WordAdderForm(props) {
         fetch('https://www.dictionaryapi.com/api/v3/references/collegiate/json/' + word + '?key=4013f53d-9186-47c1-acb8-17287689092c')
                 .then((response) => response.json())
                 .then((data) => {
+                    < NewWord wordData={data} />
                     console.log(data);
                         let word = data[0].meta.id;
                         let partOfSpeech = data[0].fl;
@@ -17,20 +19,6 @@ function WordAdderForm(props) {
                         let definition3 = data[0].shortdef[2];
                     console.log('YOUR WORD IS', word, "WHICH MEANS", definition1, 'OR', definition2, 'OR', definition3, ". IT IS A ", partOfSpeech, "WITH ORIGINS ", etymology);
 
-
-                    // if (newDream) {
-                    //     const response = await fetch('/api/dreams', {
-                    //       method: 'POST',
-                    //       body: JSON.stringify(newDream),
-                    //       headers: { 'Content-Type': 'application/json' },
-                    //     });
-                  
-                    //     if (response.ok) {
-                    //       document.location.replace('/mydreams');
-                    //     } else {
-                    //       alert('Failed to post your new dream.');
-                    //     }
-                    //   }
                 })
                 .catch((err) => {
                     console.log(err.message);
